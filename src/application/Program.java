c1fd739a28ef5ae2c2c4b04259e7680b3dccbd7f
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2024-07-17 17:08:19 -0300

    Criado metodo toString, criado entradas e saidas do Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Locale;
 import java.util.Scanner;
+import entities.Account;
 
 public class Program {
 	public static void main(String[]args) {
@@ -10,7 +11,26 @@ public class Program {
 		Scanner sc = new Scanner (System.in);
 		
 		
+		System.out.print("Enter account number: ");
+		int number = sc.nextInt();
+		System.out.print("Enter account holder: ");
+		sc.nextLine();
+		String name = sc.nextLine();
+		Account account ;
 		
+		System.out.print("Is there na initial deposit (y/n)?");
+		char resp = sc.next().charAt(0);
+		
+		if(resp == 'y') {
+			System.out.print("Enter initial deposit value:");
+			double initialDeposit = sc.nextDouble();
+			account = new Account(number, name, initialDeposit);
+		}
+		else  {
+			account = new Account(number, name);
+		} 
+		
+		System.out.println(account);
 		
 		
 		sc.close();
--- src/entities/Account.java
+++ src/entities/Account.java
@@ -3,14 +3,22 @@ package entities;
 public class Account {
 	private Integer number;
 	private String holder;
-	private Double balance;
+	private Double balance = 0.0;
 
 	
+	
+	public Account() {
+	}
+
+	public Account(Integer number, String holder, Double initialDesposit) {
+		this.number = number;
+		this.holder = holder;
+		deposit(initialDesposit);
+	}
 
 	public Account(Integer number, String holder) {
 		this.number = number;
 		this.holder = holder;
-		balance = 0.0 ;
 	}
 
 	public Integer getNumber() {
@@ -32,14 +40,19 @@ public class Account {
 	public Double getBalance() {
 		return balance;
 	}
-	
-	
-	public void deposit (double amount) {
+
+	public void deposit(double amount) {
 		balance += amount;
 	}
-	
-	public void withdraw (double amount) {
-	    balance -= amount + 5.0;
+
+	public void withdraw(double amount) {
+		balance -= amount + 5.0;
 	}
-	
+
+	public String toString() {
+		return "Account " + number
+				+ ", Holder: " + holder
+				+ String.format(", Balance : $ %.2f", balance);
+	}
+
 }
